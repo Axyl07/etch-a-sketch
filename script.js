3a652c5dd0c83f3dbee091b12a6fd0120d1a0a0b
Axyl07 <aryanchauhan000777@gmail.com>
2024-08-25 12:45:45 +0530

    Add Reset button and color on click functionality

--- script.js
+++ script.js
@@ -1,91 +1,113 @@
 const rainbow = document.querySelector('#rainbow');
-// const hover = document.querySelector('#hover');
+const hover = document.querySelector('#click');
 const colorPicker = document.querySelector('#colorPicker');
 const eraser = document.querySelector('#eraser');
 
 const container = document.querySelector("#container");
+
 function createGrid(row,col){
     for (let index = 0; index < row; index++) {
         const row = document.createElement("div");
+       
         row.className = 'row';
         row.style.display = 'flex'
         row.style.flex = '1'
         container.appendChild(row);
         for (let j = 0; j < col; j++) {
-            const col = document.createElement("div");
+            let col = document.createElement("div");
+           
             col.style.display = 'flex'
             col.style.flex = '1';
             col.className = 'col';
-            col.style.backgroundColor = 'pink'
-            col.style.border = '1px dotted black'
+            col.style.backgroundColor = 'white'
+            // col.style.border = '1px dotted black'
             // col.style.margin = '1px'
             // col.style.borderRadius = '2px'
             row.appendChild(col);
-        
-                col.addEventListener('mouseover', () => {
-                    if (rainbow.checked) {
-                        if (eraser.checked) {
-                            col.style.backgroundColor = 'pink';
-                        }else {
+            //main event for coloring the cells 
+            col.addEventListener('mouseover', (e) => {
+                if (darkening.checked) {  
+                if(click.checked){
+                if(e.buttons){
+                if (rainbow.checked) {
+                    if (eraser.checked) {
+                        col.style.backgroundColor = 'white';
+                    }else {
                         max = 255
                         let red = Math.floor(Math.random() * max);
                         let blue = Math.floor(Math.random() * max);
                         let green = Math.floor(Math.random() * max);
                         col.style.backgroundColor = "rgb("+red+", "+blue+", "+green+")";
-                        }
-                    }else {
-                        col.style.backgroundColor = `${colorPicker.value}`;
-                        if (eraser.checked) {
-                            col.style.backgroundColor = 'pink';
-                        }else col.style.backgroundColor = `${colorPicker.value}`;
+                        // col.style.opacity = "0.1"
+                        // col.style.opacity += "0.1";
                         
                     }
+                }else {
+                    col.style.backgroundColor = `${colorPicker.value}`;
+                    // col.style.opacity = "0.1"
+                    // col.style.opacity += "0.1";
+                    if (eraser.checked) {
+                        col.style.backgroundColor = 'white';
+                    }else col.style.backgroundColor = `${colorPicker.value}`;
+                    
+                }
+            }
+        } else {
+            if (rainbow.checked) {
+                if (eraser.checked) {
+                    col.style.backgroundColor = 'white';
+                }else {
+                    max = 255
+                    let red = Math.floor(Math.random() * max);
+                    let blue = Math.floor(Math.random() * max);
+                    let green = Math.floor(Math.random() * max);
+                    col.style.backgroundColor = "rgb("+red+", "+blue+", "+green+")";
+                    // col.style.opacity = "0.1"
+                    // col.style.opacity += "0.1";
+                }
+            }else {
+                col.style.backgroundColor = `${colorPicker.value}`;
+                // col.style.opacity = "0.1"
+                // col.style.opacity += "0.1";
+                if (eraser.checked) {
+                    col.style.backgroundColor = 'white';
+                }else col.style.backgroundColor = `${colorPicker.value}`;
+                
+            }
+        }
+    }
+            })
+      
+                        }
+                    }
+    
+                }
+                
+                createGrid(32,32);
+                //change grid size with user input
+                const changeSize = document.querySelector("#change-size");
+                changeSize.textContent = 'Change Size';
+                changeSize.addEventListener('click', () => {
+                    while (container.firstChild) {
+                        container.removeChild(container.firstChild)
+                    }
+                    alert ("How to Use : For a 16x16 grid enter size = 16")
+                    let size = prompt("Enter size of grid (2->100)");
+                    if (size <=100) {
+                        createGrid(size,size);
+                    }else alert("Don't use values over 100 or smaller than 2");
+                })
+                
+                
+                // Clear all cells button
+                const clear = document.querySelector('#clear');
+                clear.textContent = 'Clear Sketchpad'
+                clear.addEventListener('click',()=>{
+                    const allCols = document.querySelectorAll(".col");
+            allCols.forEach((node)=>{
+            node.style.backgroundColor = 'white';
+                })
+                console.log(allCols);
                     
                 })
-            
-            // else if (eraser.checked) {
-            //     col.addEventListener('mouseover',()=>{
-            //         if (rainbow.checked) {
-            //             max = 255
-            //             let red = Math.floor(Math.random() * max);
-            //             let blue = Math.floor(Math.random() * max);
-            //             let green = Math.floor(Math.random() * max);
-            //             col.style.backgroundColor = "rgb("+red+", "+blue+", "+green+")";
-            //         }else {
-            //             col.style.backgroundColor = "pink";
-                        
-            //         }
-            //     })
-            // }
-        }
-        }
-    }
-
-createGrid(16,16);
-
-
-const buttonDiv = document.querySelector("#buttonDiv");
-const resetButton = document.createElement("button");
-resetButton.textContent = 'Reset';
-resetButton.addEventListener('click', () => {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
-    alert ("How to Use : For a 16x16 grid enter size = 16")
-    let size = prompt("Enter size of grid (2->100)");
-        if (size <=100) {
-            createGrid(size,size);
-        }else alert("Don't use values over 100 or smaller than 2");
-    })
-    
-    buttonDiv.appendChild(resetButton);
-
-
-    const clear = document.createElement('button');
-    clear.textContent = 'Clear Sketchpad'
-    clear.addEventListener('click',()=>{
-        const allCols = document.querySelectorAll(col);
-        allCols.backgroundColor = 'pink'
-
-    })
-    buttonDiv.appendChild(clear);
+                
